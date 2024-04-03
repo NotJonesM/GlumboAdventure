7d156f2dc628c0e7fd8ab0ea1adca793de2b9e40
unknown <jonahmolinoff@gmail.com>
2024-04-03 10:27:21 -0400

    added player

--- Dungeon.java
+++ Dungeon.java
@@ -1,5 +1,9 @@
-class Dungeon {
-    public static void main(String[] args) {
+import java.util.ArrayList;
 
+class Dungeon {
+    ArrayList<ArrayList<Room>> dungeon = new ArrayList<>();
+
+    private void printMap(ArrayList<Room> map){
+        System.out.println(map);
     }
 }
