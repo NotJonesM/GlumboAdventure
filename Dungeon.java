b9cf7e0cba7dc33e6822c3a2bf2727b9394d3654
Bram Philip <bramphilip@gmail.com>
2024-04-03 11:29:53 -0400

    dungeon changes

--- Dungeon.java
+++ Dungeon.java
@@ -1,9 +1,37 @@
 import java.util.ArrayList;
 
 class Dungeon {
-    ArrayList<ArrayList<Room>> dungeon = new ArrayList<>();
+    // Fields
+    private ArrayList<ArrayList<Room>> dungeon = new ArrayList<>();
+    private String name;
+    private ArrayList<Item> possibleItems = new ArrayList<>();
+    private ArrayList<Monster> possibleMonsters = new ArrayList<>();
+    private int entranceX;
+    private int entranceY;
+    private int exitX;
+    private int exitY;
 
+    // Constructor
+    public Dungeon(String name, ArrayList<Item> possibleItems, ArrayList<Monster> possibleMonsters, int entranceX, int entranceY, int exitX, int exitY) {
+        this.name = name;
+        this.possibleItems = possibleItems;
+        this.possibleMonsters = possibleMonsters;
+    }
+
+    // Accessors
+    public int getLength() { return dungeon.size(); }
+    public int getWidth() { return dungeon.get(0).size(); } // assumes dungeon is not a ragged array
+
+    // Methods
     private void printMap(ArrayList<Room> map){
         System.out.println(map);
+    }
+
+    public void populateMap() {
+        for (ArrayList<Room> row : dungeon) {
+            for (Room r : row) {
+                // TODO: add items/monsters to room
+            }
+        } 
     }
 }
