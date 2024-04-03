d02f03c6c1167b50de10214a9d3c0cf065adb588
Bram Philip <bramphilip@gmail.com>
2024-04-03 12:51:15 -0400

    update runner.java

--- Runner.java
+++ Runner.java
@@ -7,7 +7,13 @@ public class Runner {
         Scanner scan = new Scanner(System.in);
 
         while(true) { // ###### MAIN RUNNER LOOP #####
-            
+            System.out.print("Enter a command: ");
+            String in = scan.nextLine();
+            if (GlumboToolbox.matchStart(in, "GRAB")) { /* TODO add get item functionality */}
+            else if (GlumboToolbox.matchStart(in, "ATTACK")) { /* TODO add attack functionality */}
+            else if (GlumboToolbox.matchStart(in, "MOVE")) { /* TODO add move functionality */}
+            else if (GlumboToolbox.matchStart(in, "USE")) { /* TODO add use functionality */}
+            else { System.out.println("Command not recognized!"); }
         }
     }
 }
