34886ac23830872364d04e8922904a43fec452a0
Bram Philip <bramphilip@gmail.com>
2024-04-03 10:57:15 -0400

    renamed methods in item hierarchy + coded door class

--- Door.java
+++ Door.java
@@ -1,3 +1,20 @@
 public class Door {
-    
+    // Fields
+    private String type; // type of door, for descriptions' sake, e.g., wood, stone, arched, etc. 
+    private boolean exit;
+
+    // Constructor(s)
+    public Door(String type, boolean exit) {
+        this.type = type;
+        this.exit = exit;
+    }
+
+    // Accessors
+    public String getType() { return type; }
+    public boolean isExit() { return exit; }
+
+    // Methods
+    public String toString() {
+        return "a " + getType() + " door";
+    }
 }
